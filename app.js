587cd82e077782030ee0c3f5200a4dd3ac4e9bf8
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-10-17 22:02:26 +0500

    addEventListener

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
-// let btn = document.querySelector("button");
-// console.dir(btn);
+// // let btn = document.querySelector("button");
+// // console.dir(btn);
 
-//but if we want to click on mutiple buttons then
-let btns = document.querySelectorAll("button");
-console.dir(btns);
+// //but if we want to click on mutiple buttons then
+// let btns = document.querySelectorAll("button");
+// console.dir(btns);
 
 
-// btn.onclick = function(){
-//     console.log("button was clicked ");
+// // btn.onclick = function(){
+// //     console.log("button was clicked ");
+// // };
+
+// // or we can write like this
+// function sayHello(){
+//     alert("Hello");
 // };
 
-// or we can write like this
-function sayHello(){
-    alert("Hello");
-};
+// //btns.onclick = sayHello;
+// // now as we have many buttons so we use loop to set onclick property on each
+// for(btn of btns){
+//     btn.onclick = sayHello;
+//     btn.onmouseenter = function(){
+//         console.log("you clicked button");
+//     }
+// }
 
-//btns.onclick = sayHello;
-// now as we have many buttons so we use loop to set onclick property on each
+
+
+
+            // Event Listeners
+    // we have learned two methods , inline event in html like onclick and onmouseenter
+    // second was creating functions for event occurance 
+    // third now we will learn Event Listeners
+    // addEventListener
+    // element.addEventListener(event, callback);
+    // event can be click, drag or key press etc.
+
+let btns = document.querySelectorAll("button");
 for(btn of btns){
-    btn.onclick = sayHello;
-    btn.onmouseenter = function(){
-        console.log("you clicked button");
-    }
+   // console.dir(btn);
+    btn.addEventListener("click", sayHello);
+    btn.addEventListener("click", sayName);
+}
+
+function sayHello(){
+    console.log("hello");
+}
+
+function sayName(){
+    console.log("Bheesham");
 }
